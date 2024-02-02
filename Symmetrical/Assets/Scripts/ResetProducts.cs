85b3da7ede8bd6f55a72098286f5a2b072d70f7b
Pedro A. Ramos <67925461+PedroA-78@users.noreply.github.com>
2024-02-02 18:13:18 -0300

    Reset Products
    - Adicionada uma função p/ retirar um produto da prateleira
    - Adicionada uma função p/ repor os produtos na prateleira (  sem animação )
    - Player, Floor e Gondola foram transformados em prefabs
    - Criado alguns produtos p/ testes ( Cube, Sphere e Capsule )

---
+++ Symmetrical/Assets/Scripts/ResetProducts.cs
@@ -0,0 +1,141 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
+using UnityEngine;
+public class ResetProducts : MonoBehaviour
+{
+    public GameObject product, gondola;
+    GameObject shelfChild;
+    public float time = 3f;
+    private float initTime;
+    private int index;
+    List<GameObject> shelfs = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < gondola.transform.childCount; i++) {
+            if (gondola.transform.GetChild(i).ToString().Contains("Shelf")) {
+                shelfs.Add(gondola.transform.GetChild(i).transform.gameObject);
+            }
+        }
+
+        initTime = time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) {
+            if (shelfs.Count <= 1) {
+                Debug.Log("Vazio");
+                init();
+            } else {
+                Debug.Log("Cheio");
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            GameObject shelf = selectShelf();
+            GameObject product;
+
+            if (shelf == null) {
+                return;
+            } else {
+                product = selectProduct(shelf);
+            }
+
+            Destroy(product);
+        }
+
+        time -= Time.deltaTime;
+        if (time <= 0f) {
+            GameObject shelf = selectShelf();
+            GameObject product;
+
+            if (shelfs.Count <= 0) {
+                Debug.Log("Vazio");
+                init();
+            }
+
+            if (shelf == null) {
+                return;
+            } else {
+                product = selectProduct(shelf);
+            }
+
+            Destroy(product);
+
+            time = initTime;
+        }
+    }
+
+    public GameObject selectProduct(GameObject shelf) {
+        List<GameObject> products = new List<GameObject>();
+
+        for (int i = 0; i < shelf.transform.childCount; i++) {
+            GameObject product = shelf.transform.GetChild(i).transform.gameObject;
+
+            if (product.transform.ToString().Contains("Cube F")) {
+                products.Add(product);
+            }
+        }
+
+        if (products.Count <= 0) {
+            for (int i = 0; i < shelf.transform.childCount; i++) {
+                products.Add(shelf.transform.GetChild(i).transform.gameObject);
+            }
+        }
+
+        return products[Random.Range(0, products.Count)];
+    }
+
+    public GameObject selectShelf() {
+        int index;
+        while (shelfs.Count > 0) {
+            index = Random.Range(0, shelfs.Count);
+            if (!emptyShelf(shelfs[index])) {
+                return shelfs[index];
+            } else {
+                shelfs.RemoveAt(index);
+            }
+
+        }
+
+        return null;
+    }
+
+    public bool emptyShelf(GameObject shelf) {
+        if (shelf.transform.childCount <= 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void init() {
+
+        GameObject shelf;
+        float coordX = 1f;
+
+        for (int i = 0; i <= 3; i++) {
+            shelf = gondola.transform.Find("Shelf " + i).transform.gameObject;
+            for (int y = 0; y <= 4; y++) {
+                GameObject productF = Instantiate(product);
+                GameObject productB = Instantiate(product);
+
+                productF.name = "Cube F" + y;
+                productB.name = "Cube B" + y;
+
+                productF.transform.SetParent(shelf.transform);
+                productF.transform.localPosition = new Vector3(coordX,0,.25f);
+
+                productB.transform.SetParent(shelf.transform);
+                productB.transform.localPosition = new Vector3(coordX,0,-.25f);
+                
+                coordX -= .5f;
+            }
+                shelfs.Add(gondola.transform.GetChild(i + 1).transform.gameObject);
+                coordX = 1f;
+        }
+    }
+}
